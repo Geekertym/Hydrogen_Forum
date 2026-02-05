5c4cdd297117ecbb083102b73fdf52538206e9eb
Geekertym <tym20120721@126.com>
2026-02-05 22:06:33 +0800

    third commit

---
+++ pages/js/index.js
@@ -0,0 +1,19 @@
+<script>
+    // 初始化SimpleMDE Markdown编辑器
+    var simplemde = new SimpleMDE({
+        element: document.getElementById("markdown-input"),
+    });
+    
+
+
+
+
+
+
+
+
+
+
+
+
+</script>
